7a5c517c9e94d26581375d9491d4de5f6493e8ad
Ruslan <ruslanpunin@yande.ru>
2020-04-09 21:43:24 +0300

    checked checkbox in view editPage

--- src/main/java/com/example/springboot/controller/AdminController.java
+++ src/main/java/com/example/springboot/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -17,6 +18,7 @@ public class AdminController {
 
 	@Autowired
 	private UserService service;
+
 
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
@@ -29,15 +31,24 @@ public class AdminController {
 
 		return modelAndView;
 	}
+	@RequestMapping(value="/edit", method = RequestMethod.GET)
+	public ModelAndView editPage() {
+		List<Role> listRoles = service.getAllRole();
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("listRoles", listRoles);
+		modelAndView.setViewName("admin/addPage");
+		return modelAndView;
+	}
 
-	@RequestMapping(value="/edit/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/edit/{id}",  method = RequestMethod.GET)
 	public ModelAndView editPage(@PathVariable("id") Long id) {
 		User user = service.getUserById(id);
-
+		List<Role> listRoles = service.getAllRole();
 
 		ModelAndView modelAndView = new ModelAndView();
 
 		modelAndView.setViewName("admin/editPage");
+		modelAndView.addObject("listRoles", listRoles);
 		modelAndView.addObject("user", user);
 
 		return modelAndView;
@@ -45,24 +56,37 @@ public class AdminController {
 
 	@RequestMapping(value = "/edit", method = RequestMethod.POST)
 	public ModelAndView editUser(@ModelAttribute("user") User user, @RequestParam("checkboxRole") String[] checkboxRoles) {
+		List<Role> listRoles = service.getAllRole();
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("redirect:/admin/users");
+		List<Role> oldRoles = new ArrayList<>();
+		for (int i = 0; i < checkboxRoles.length; i++) {
+			oldRoles.add(new Role(checkboxRoles[i]));
+		}
+		user.setRoles(oldRoles);
 		if (checkboxRoles.length < 2) {
-			modelAndView.addObject("error","Choose the role or roles");
 			modelAndView.setViewName("admin/editPage");
+			modelAndView.addObject("listRoles", listRoles);
+			modelAndView.addObject("error","Choose the role or roles");
+			modelAndView.addObject("user", user);
 			return modelAndView;
 		}
 
 		if (user.getPassword().equals("") || user.getEmail().equals("")  || user.getName().equals("") ) {
-			modelAndView.addObject("error","Some field is empty");
 			modelAndView.setViewName("admin/editPage");
+			modelAndView.addObject("listRoles", listRoles);
+			modelAndView.addObject("error","Some field is empty");
+			modelAndView.addObject("user", user);
 			return modelAndView;
 		}
 		String oldUnicEmail = service.getUserById(user.getId()).getEmail();
 
 		if ((!service.unicEmail(user.getEmail()) ^ user.getEmail().equals(oldUnicEmail))) {
-			modelAndView.addObject("error","Email must be unic");
 			modelAndView.setViewName("admin/editPage");
+			modelAndView.addObject("user", user);
+			modelAndView.addObject("listRoles", listRoles);
+			modelAndView.addObject("error","Email must be unic");
+
 			return modelAndView;
 		}
 
@@ -81,20 +105,25 @@ public class AdminController {
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public ModelAndView addUser(@ModelAttribute("user") User user, @RequestParam("checkboxRole") String[] checkboxRoles) {
+		List<Role> listRoles = service.getAllRole();
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("redirect:/admin/users");
 		if (checkboxRoles.length < 2) {
 			modelAndView.addObject("error","Choose the role or roles");
+
+			modelAndView.addObject("listRoles", listRoles);
 			modelAndView.setViewName("admin/addPage");
 			return modelAndView;
 		}
 		if (user.getPassword().equals("") || user.getEmail().equals("")  || user.getName().equals("") ) {
 			modelAndView.addObject("error","Some field is empty");
+			modelAndView.addObject("listRoles", listRoles);
 			modelAndView.setViewName("admin/addPage");
 			return modelAndView;
 		}
 		if (!service.unicEmail(user.getEmail())) {
 			modelAndView.addObject("error","Email must be unic");
+			modelAndView.addObject("listRoles", listRoles);
 			modelAndView.setViewName("admin/editPage");
 			return modelAndView;
 		}
